23170d7ad0db21b083fd652c9320de0e17d3113a
Shoubhit Dash <shoubhit2005@gmail.com>
2022-04-12 18:38:42 +0530

    add tailwind to root

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import styles from "./styles/app.css";
 import {
   Links,
   LiveReload,
@@ -13,6 +14,10 @@ export const meta: MetaFunction = () => ({
   title: "New Remix App",
   viewport: "width=device-width,initial-scale=1",
 });
+
+export function links() {
+  return [{ rel: "stylesheet", href: styles }];
+}
 
 export default function App() {
   return (

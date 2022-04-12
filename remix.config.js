151c783a771f569f9bd27ed93fdf601c375c284d
Shoubhit Dash <shoubhit2005@gmail.com>
2022-04-12 18:21:31 +0530

    configure remix to import rehype

--- remix.config.js
+++ remix.config.js
@@ -8,8 +8,16 @@ module.exports = {
   // so we default back to the standard build output.
   server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
   ignoredRouteFiles: [".*"],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "api/index.js",
-  // publicPath: "/build/",
+  appDirectory: "app",
+  assetsBuildDirectory: "public/build",
+  serverBuildPath: "api/index.js",
+  publicPath: "/build/",
+  mdx: async (filename) => {
+    const [rehypeHighlight] = await Promise.all([
+      import("rehype-highlight").then((module) => module.default),
+    ]);
+    return {
+      rehypePlugins: [rehypeHighlight],
+    };
+  },
 };

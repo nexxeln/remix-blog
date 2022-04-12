f51c3569a4f2411fb1be036fe664dcb7162960d4
Shoubhit Dash <shoubhit2005@gmail.com>
2022-04-12 20:31:45 +0530

    add meta tag to main page

--- app/routes/blog.tsx
+++ app/routes/blog.tsx
@@ -1,6 +1,13 @@
-import type { LinksFunction } from "@remix-run/node";
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
 import { Outlet } from "@remix-run/react";
 import styles from "highlight.js/styles/github-dark-dimmed.css";
+
+export const meta: MetaFunction = () => {
+  return {
+    title: "nexxel's blog",
+    description: "here nexxel writes about stuff",
+  };
+};
 
 export const links: LinksFunction = () => {
   return [

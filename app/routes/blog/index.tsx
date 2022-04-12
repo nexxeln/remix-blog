cf50ed086ef634d005c05e6c65c0bc16c22467cf
Shoubhit Dash <shoubhit2005@gmail.com>
2022-04-12 20:05:35 +0530

    make page for listing blogs

---
+++ app/routes/blog/index.tsx
@@ -0,0 +1,41 @@
+import type { LoaderFunction } from "@remix-run/node";
+import * as goGol from "go-gol.mdx";
+import * as nexdle from "nexdle.mdx";
+import * as genLicense from "gen-license.mdx";
+import { Link, useLoaderData } from "@remix-run/react";
+
+function postFromModule(module: any) {
+  return {
+    slug: module.filename.replace(/\.mdx?$/, ""),
+    ...module.attributes.meta,
+  };
+}
+
+export const loader: LoaderFunction = () => {
+  return [
+    postFromModule(goGol),
+    postFromModule(nexdle),
+    postFromModule(genLicense),
+  ];
+};
+
+export default function BlogIndex() {
+  const posts = useLoaderData();
+  return (
+    <div>
+      <h2>Posts</h2>
+
+      <ul>
+        {posts.map((post: any) => (
+          <li key={post.slug}>
+            <Link to={`/blog/${post.slug}`}>{post.title}</Link>
+
+            {post.description && (
+              <p className="m-0 lg:m-0">{post.description}</p>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}

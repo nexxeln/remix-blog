d327305cea50f99bd7c8e6ae1ebc2fa647e15b46
Shoubhit Dash <shoubhit2005@gmail.com>
2022-04-12 20:48:01 +0530

    create landing page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,18 @@
+import { Link } from "@remix-run/react";
+
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>nexxel's blog</h1>
+    <div className="flex justify-center items-center text-center text-4xl px-6 h-screen">
+      <div>
+        <h1 className="font-bold">Welcome to my bare-bones blog</h1>
+        <Link to={"/blog"}>
+          <button className="pt-6">
+            <span className="font-normal text-xl bg-black text-white px-4 py-2 hover:opacity-90 transition-opacity duration-300 rounded-sm shadow-2xl">
+              Go to the blog
+            </span>
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }

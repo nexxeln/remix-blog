b75b71dfdf69e8f762b1945fe2f9590df7828c8c
Shoubhit Dash <shoubhit2005@gmail.com>
2022-04-13 21:31:32 +0530

    change landing page text

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -12,9 +12,7 @@ export default function Index() {
   return (
     <div className="flex items-center justify-center h-screen px-6 text-4xl text-center">
       <div>
-        <h1 className="font-black font-Karla">
-          Welcome to my bare-bones blog.
-        </h1>
+        <h1 className="font-black font-Karla">Welcome to nexxel's blog.</h1>
         <Link to={"/blog"}>
           <button className="px-5 py-2 mt-5 text-base font-bold text-gray-800 bg-white border border-gray-400 rounded shadow font-Karla hover:bg-gray-100">
             Go to the blog!
